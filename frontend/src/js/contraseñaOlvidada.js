15b55ef2048d1e31d9f82441f867383b2213e509
alvarogomez05 <alvatox11@gmail.com>
2025-02-04 21:13:53 +0100

    html y estilos de solicitar contraseña

---
+++ frontend/src/js/contraseñaOlvidada.js
@@ -0,0 +1,20 @@
+let contrasena_olvidada = document.getElementById("contrasena_olvidada");
+
+
+
+let IrReestablecer = () =>{
+    location.href="./pages/forgettenpass.html"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+contrasena_olvidada.addEventListener("click",IrReestablecer)
