fd642264b509c2d5fa2ded8f12c66d0ca616cf2e
alvarogomez05 <alvatox11@gmail.com>
2025-02-18 16:39:15 +0100

    arreglos con las imagenes

--- frontend/src/js/cassetes.js
+++ frontend/src/js/cassetes.js
@@ -1,4 +1,4 @@
-const organosHumanos = [   "Cerebro",
+const organosHumanos = ["Cerebro",
   "Cerebelo",
   "Tronco encefalico",
   "Medula espinal",
@@ -44,12 +44,12 @@ let carac = document.getElementById("caracteristicas")
 let ob = document.getElementById("observaciones")
 let submitCrearCassete = document.getElementById("submitCrearCassete")
 //modal modificar Cassetes
-let desM=document.getElementById("descripcionMod")
-let dateM=document.getElementById("fechaMod")
-let selecOrMod=document.getElementById("organoMod")
-let caracMod=document.getElementById("caracteristicasMod")
-let obMod=document.getElementById("observacionesMod")
-let submitModCassete=document.getElementById("submitModificarCassete")
+let desM = document.getElementById("descripcionMod")
+let dateM = document.getElementById("fechaMod")
+let selecOrMod = document.getElementById("organoMod")
+let caracMod = document.getElementById("caracteristicasMod")
+let obMod = document.getElementById("observacionesMod")
+let submitModCassete = document.getElementById("submitModificarCassete")
 // modal nuevos cassettes
 let nuevoCassete = document.getElementById('nuevoCassete');
 let cerrarNuevoCassete = document.getElementById('cerrarNuevoCassete');
@@ -64,9 +64,9 @@ let cancelDelete = document.getElementById('cancelDelete')
 let confirmDelete = document.getElementById('confirmDelete')
 let eliminarCassete = document.getElementById('eliminarCassete')
 //botones para ordenar
-let fechaBoton=document.getElementById("fechaBoton")
-let descripcionBoton=document.getElementById("descripcionBoton")
-let organoBoton=document.getElementById("organoBoton")
+let fechaBoton = document.getElementById("fechaBoton")
+let descripcionBoton = document.getElementById("descripcionBoton")
+let organoBoton = document.getElementById("organoBoton")
 
 // Función para agregar eventos solo si el elemento existe
 function addEventListenerIfExists(id, event, callback) {
@@ -225,91 +225,91 @@ const detalleCassete = async (event) => {
 }
 
 //Funcion para borrar el cassete seleccionado
-const borrarCassete=async()=>{
-  console.log("El id ha borrar es: "+id)
-  const delete_cassete=await fetch(`http://localhost:5001/api/cassete/${id}`,{
-    method:'DELETE',
+const borrarCassete = async () => {
+  console.log("El id ha borrar es: " + id)
+  const delete_cassete = await fetch(`http://localhost:5001/api/cassete/${id}`, {
+    method: 'DELETE',
   })
-  if(delete_cassete.ok){
+  if (delete_cassete.ok) {
     mostrarCassetes()
     location.reload()
   }
 }
 //Funcion para saber si se ha hecho click previamente en algun cassete y si no, mostrar el error
-const comprobarBorrado=()=>{
-  if(id){
+const comprobarBorrado = () => {
+  if (id) {
     mostrar(deleteModal)
-    
-  }else{
-      cassetteDetail.textContent="NO HAS SELECCIONADO NADA MACHO"
+
+  } else {
+    cassetteDetail.textContent = "NO HAS SELECCIONADO NADA MACHO"
   }
 }
 
 //Funcion para modificar el cassete
-const modCassete=async()=>{
+const modCassete = async () => {
 
   let descripcion = desM.value
   let fecha = dateM.value
   let organo = selecOrMod.value
   let caracteristicas = caracMod.value
   let observaciones = obMod.value
-    const modificar=await fetch(`http://localhost:5001/api/cassete/${id}`,{
-        method:'PUT',
-        body: JSON.stringify({ descripcion, fecha, organo, caracteristicas, observaciones, usuario_id }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
-    if(modificar.ok){
-      mostrarCassetes()
-      location.reload()
-    }
+  const modificar = await fetch(`http://localhost:5001/api/cassete/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({ descripcion, fecha, organo, caracteristicas, observaciones, usuario_id }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+  if (modificar.ok) {
+    mostrarCassetes()
+    location.reload()
+  }
 }
 
 //Funcion para comprobar si hay algun cassete seleccionado para mostrar el modal de modificar
-const comprobarActualizacion=async()=>{
-  if(id){
+const comprobarActualizacion = async () => {
+  if (id) {
     mostrar(modalModificarCassette)
-    const api=await peticionApiID(id)
-   
-    desM.value=api.descripcion
+    const api = await peticionApiID(id)
+
+    desM.value = api.descripcion
     let fechaTexto = api.fecha ? api.fecha.toString().substring(0, 10) : "Fecha no disponible";
-    dateM.value=fechaTexto
-    selecOrMod.value=api.organo
-    caracMod.textContent=api.caracteristicas
-    obMod.textContent=api.observaciones
-  }else{
-    cassetteDetail.textContent="NO HAS SELECCIONADO NADA MACHO"
-}
+    dateM.value = fechaTexto
+    selecOrMod.value = api.organo
+    caracMod.textContent = api.caracteristicas
+    obMod.textContent = api.observaciones
+  } else {
+    cassetteDetail.textContent = "NO HAS SELECCIONADO NADA MACHO"
+  }
 }
 //Inicializamos Sorttable para la tabla con los cassetes mostrados
 const Sortable = window.Sortable;
-const ordenarFecha=()=>{
+const ordenarFecha = () => {
   console.log("Ordenar")
   let rows = Array.from(listaCassetes.rows).slice(1); // Obtener las filas de datos, ignorando el encabezado
   console.log(rows)
   // Ordenar las filas por la fecha (columna 0)
-  rows.sort(function(a, b) {
+  rows.sort(function (a, b) {
     var fechaA = new Date(a.cells[0].textContent); // Obtener la fecha de la primera celda
     var fechaB = new Date(b.cells[0].textContent);
     return fechaA - fechaB; // Ordenar de más antiguo a más reciente
   });
 
   // Reinsertar las filas ordenadas en la tabla
-  rows.forEach(function(row) {
+  rows.forEach(function (row) {
     listaCassetes.appendChild(row);
   });
 }
-const ordenarDescripcion=()=>{
+const ordenarDescripcion = () => {
   console.log("Descripcion")
   let rows = Array.from(listaCassetes.rows).slice(1); // Obtener las filas de datos, ignorando el encabezado
   console.log(rows)
-  rows.sort(function(a, b) {
+  rows.sort(function (a, b) {
 
-    var valorA =a.cells[1].textContent // Obtener la fecha de la primera celda
+    var valorA = a.cells[1].textContent // Obtener la fecha de la primera celda
     var valorB = b.cells[1].textContent
-    console.log("fecha A:"+valorA)
-    console.log("fecha B:"+valorB)
+    console.log("fecha A:" + valorA)
+    console.log("fecha B:" + valorB)
     // Ordenar alfabéticamente
     if (valorA < valorB) return -1;
     if (valorA > valorB) return 1;
@@ -318,21 +318,21 @@ const ordenarDescripcion=()=>{
 
 
   // Reinsertar las filas ordenadas en la tabla
-  rows.forEach(function(row) {
+  rows.forEach(function (row) {
     listaCassetes.appendChild(row);
   });
 }
 
-const ordenarOrgano=()=>{
+const ordenarOrgano = () => {
   console.log("Organo")
   let rows = Array.from(listaCassetes.rows).slice(1); // Obtener las filas de datos, ignorando el encabezado
   console.log(rows)
-  rows.sort(function(a, b) {
+  rows.sort(function (a, b) {
 
-    var valorA =a.cells[2].textContent // Obtener la fecha de la primera celda
+    var valorA = a.cells[2].textContent // Obtener la fecha de la primera celda
     var valorB = b.cells[2].textContent
-    console.log("fecha A:"+valorA)
-    console.log("fecha B:"+valorB)
+    console.log("fecha A:" + valorA)
+    console.log("fecha B:" + valorB)
     // Ordenar alfabéticamente
     if (valorA < valorB) return -1;
     if (valorA > valorB) return 1;
@@ -341,7 +341,7 @@ const ordenarOrgano=()=>{
 
 
   // Reinsertar las filas ordenadas en la tabla
-  rows.forEach(function(row) {
+  rows.forEach(function (row) {
     listaCassetes.appendChild(row);
   });
 }
@@ -353,7 +353,7 @@ document.addEventListener("DOMContentLoaded", () => {
   organosHumanos.forEach(organo => {
     let option = document.createElement("OPTION")
     let option2 = document.createElement("OPTION")
-    let option3=document.createElement("OPTION")
+    let option3 = document.createElement("OPTION")
     option.textContent = organo
     option.value = organo.trim();
     option2.textContent = organo
@@ -366,13 +366,13 @@ document.addEventListener("DOMContentLoaded", () => {
   })
 })
 submitCrearCassete.addEventListener("click", crearCassete)
-confirmDelete.addEventListener("click",borrarCassete)
-eliminarCassete.addEventListener("click",comprobarBorrado)
+confirmDelete.addEventListener("click", borrarCassete)
+eliminarCassete.addEventListener("click", comprobarBorrado)
 modificarCassete.addEventListener('click', comprobarActualizacion)
-submitModificarCassete.addEventListener("click",modCassete)
-fechaBoton.addEventListener("click",ordenarFecha)
-descripcionBoton.addEventListener("click",ordenarDescripcion)
-organoBoton.addEventListener("click",ordenarOrgano)
+submitModificarCassete.addEventListener("click", modCassete)
+fechaBoton.addEventListener("click", ordenarFecha)
+descripcionBoton.addEventListener("click", ordenarDescripcion)
+organoBoton.addEventListener("click", ordenarOrgano)
 // A PARTIR DE AQUI ALVARO
 // ARREGLO DE LAS MODALES
 
@@ -696,30 +696,32 @@ const peticionImagenesMuestra = async (id) => {
     console.log("JSON recibido:", data); // Verifica qué se recibe
 
     if (data.length == 0) {
+      containerImg__detalleMuestra.innerHTML = ""
       cargarImagenPorDefecto();
       return;
+    } else {
+      // Convertir el buffer en un Blob
+      // ahora mismo esto sube la primera foto en grande
+      const uint8Array = new Uint8Array(data[0].imagen.data);
+      // console.log(uint8Array)
+      const blob = new Blob([uint8Array], { type: "image/png" }); // Cambia el tipo según el formato de tu imagen
+      const imageUrl = URL.createObjectURL(blob);
+
+      cargarImagenesMuestra(imageUrl, data[0].id);
+
+      // para mostrar todas las imagenes pequeñas
+      data.forEach((img) => {
+        // console.log(img)
+        const uint8Array = new Uint8Array(img.imagen.data);
+        const blob = new Blob([uint8Array], { type: "image/png" });
+        const imageUrl = URL.createObjectURL(blob);
+        mostrarOpcionesImagenes(imageUrl, img.id);
+      });
+      // primero se vacia para que no se sobreescriba
+      containerImg__detalleMuestra.append(fragment3)
+
     }
 
-    // Convertir el buffer en un Blob
-    // ahora mismo esto sube la primera foto en grande
-    const uint8Array = new Uint8Array(data[0].imagen.data);
-    // console.log(uint8Array)
-    const blob = new Blob([uint8Array], { type: "image/png" }); // Cambia el tipo según el formato de tu imagen
-    const imageUrl = URL.createObjectURL(blob);
-
-    cargarImagenesMuestra(imageUrl,data[0].id);
-
-    // para mostrar todas las imagenes pequeñas
-    data.forEach((img) => {
-      // console.log(img)
-      const uint8Array = new Uint8Array(img.imagen.data);
-      const blob = new Blob([uint8Array], { type: "image/png" }); 
-      const imageUrl = URL.createObjectURL(blob);
-      mostrarOpcionesImagenes(imageUrl,img.id);
-    });
-    // primero se vacia para que no se sobreescriba
-    containerImg__detalleMuestra.innerHTML=""
-    containerImg__detalleMuestra.append(fragment3)
 
   } catch (error) {
     console.error("Error al obtener la imagen:", error);
@@ -735,7 +737,7 @@ const cargarImagenPorDefecto = () => {
 
 // cargamos las imagenes de la muestra
 // le pasamos el id por el alt para ppoder borrarlo posteriormente 
-const cargarImagenesMuestra = (imagenes,id) => {
+const cargarImagenesMuestra = (imagenes, id) => {
   // console.log('cargar una imagen en grande y el resto en pequeño')
   // console.log(imagenes)
   let src = imagenes;
@@ -784,39 +786,39 @@ const subirImagen = async (event) => {
 
 // aqui debemos de crear las imagenes e introducirlas en un fragment, el src es (img)
 // pasamos el id oara guardarlo en el alt y asi poder borrar la imagen
-const mostrarOpcionesImagenes = (img,id) =>{
+const mostrarOpcionesImagenes = (img, id) => {
   // console.log(img)
 
   let image = document.createElement("IMG")
   image.src = img;
   image.alt = id;
-  image.classList="h-10 w-10 m-4"
+  image.classList = "h-10 w-10 m-4"
   fragment3.append(image);
 }
 
-const borrarImagen = () =>{
+const borrarImagen = () => {
   console.log(Img__detalleMuestra.alt)
 
-  let url = 'http://localhost:5001/api/imagen/'+Img__detalleMuestra.alt;
+  let url = 'http://localhost:5001/api/imagen/' + Img__detalleMuestra.alt;
 
   fetch(url, {
     method: 'DELETE',  // Especificamos que es una solicitud DELETE
     headers: {
-        'Content-Type': 'application/json', // Puedes añadir otros headers si es necesario
-        // 'Authorization': 'Bearer token' // Si necesitas autenticación, descomenta y añade el token
+      'Content-Type': 'application/json', // Puedes añadir otros headers si es necesario
+      // 'Authorization': 'Bearer token' // Si necesitas autenticación, descomenta y añade el token
     }
-}).then(response => console.log(response))
+  }).then(response => console.log(response))
 
   // ocultamos la modal de la elimiacion
   ocultar(deleteModal__img)
   ocultar(detalleMuestra__modal)
 }
 
-const cambiarImg = (event) =>{
-  if(event.target.tagName === 'IMG'){
+const cambiarImg = (event) => {
+  if (event.target.tagName === 'IMG') {
     // nos aseguramos de que pincha en una imagen
     console.log(event.target.alt)
-    cargarImagenesMuestra(event.target.src,event.target.alt)
+    cargarImagenesMuestra(event.target.src, event.target.alt)
   }
 }
 
@@ -828,8 +830,8 @@ close__detalleMuestra__modal.addEventListener('click', () => ocultar(detalleMues
 confirmDelete__muestra.addEventListener('click', () => borrado(id__muestra))
 cancelDelete__muestra.addEventListener('click', () => ocultar(deleteModal__muestra))
 close__updateMuestra__modal.addEventListener('click', () => ocultar(updateModal__muestra))
-delete__image.addEventListener('click', ()=>mostrar(deleteModal__img))
-cancelDelete__img.addEventListener('click', ()=>ocultar(deleteModal__img))
+delete__image.addEventListener('click', () => mostrar(deleteModal__img))
+cancelDelete__img.addEventListener('click', () => ocultar(deleteModal__img))
 listaMuestras.addEventListener('click', DetailMuestras)
 newMuestra__form.addEventListener('submit', createMuestra)
 delete__muestra.addEventListener('click', deleteMuestra)
@@ -838,7 +840,7 @@ updateMuestra__form.addEventListener('submit', updateMuestra)
 aniadirImg__button.addEventListener('click', () => {
   aniadirImg__detalleMuestra.click();
 })
-confirmDelete__img.addEventListener('click',borrarImagen)
+confirmDelete__img.addEventListener('click', borrarImagen)
 
 aniadirImg__detalleMuestra.addEventListener('change', subirImagen)
-containerImg__detalleMuestra.addEventListener('click',cambiarImg)
+containerImg__detalleMuestra.addEventListener('click', cambiarImg)
